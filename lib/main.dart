737d0ae8718a496c5b4b6ceae21bf7ae2499d478
Sina <SinaKordestani@gmail.com>
2024-02-25 22:07:23 +0330

    Set Font Weight Example

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,6 @@ import 'package:flutter/material.dart';
 void main() {
   // runApp(MaterialApp(home: Text("Sina TH",textAlign: TextAlign.center,style:TextStyle(color: Colors.red) ,),));
 
-  runApp(MaterialApp(home: Center(child: Text("Sina Kordestani",textAlign: TextAlign.center,style:TextStyle(color: Colors.red) ,)),));
+  runApp(MaterialApp(home: Center(child: Text("Sina Kordestani",textAlign: TextAlign.center,style:TextStyle(color: Colors.red,fontWeight: FontWeight.bold) ,)),));
 
 }
