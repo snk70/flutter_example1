d71be15420e29a25c5a2ef79a08f2d4db9e37562
Sina <SinaKordestani@gmail.com>
2024-03-23 15:06:03 +0330

    Use StateLess Widget for clean code

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,25 @@ import 'package:flutter/material.dart';
 void main() {
   // runApp(MaterialApp(home: Text("Sina TH",textAlign: TextAlign.center,style:TextStyle(color: Colors.red) ,),));
 
-  runApp(MaterialApp(home: Center(child: Text("Sina Kordestani",textAlign: TextAlign.center,style:TextStyle(color: Colors.red,fontWeight: FontWeight.bold) ,)),));
+  runApp(const MyWidget(
+    key: Key("Sina"),
+  ));
+}
 
+class MyWidget extends StatelessWidget {
+  const MyWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Center(
+          child: MaterialButton(
+              onPressed: () {},
+              child: const Text(
+                "Sina",
+                style: TextStyle(
+                    fontSize: 20, color: Color.fromARGB(255, 100, 100, 100)),
+              ))),
+    );
+  }
 }
