31927ebc5c769f917d8b166761803b6081c5a24f
Sina <SinaKordestani@gmail.com>
2024-03-23 20:44:34 +0330

    StateFull Widget Example

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_example1/mywidget.dart';
+import 'package:flutter_example1/wgt2.dart';
 
 double width = 0;
 void main() {
@@ -14,12 +15,12 @@ void main() {
     ),
   );
 
-  runApp(const MyWidget(
-    key: Key("Sina"),
-  ));
-
-  // runApp(const Wgt2(
+  // runApp(const MyWidget(
   //   key: Key("Sina"),
   // ));
+
+  runApp(const Wgt2(
+    key: Key("Sina"),
+  ));
 
 }
--- lib/wgt2.dart
+++ lib/wgt2.dart
@@ -1,11 +1,38 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
-class Wgt2 extends StatelessWidget {
+class Wgt2 extends StatefulWidget {
   const Wgt2({super.key});
 
   @override
+  State<Wgt2> createState() => _Wgt2State();
+}
+
+class _Wgt2State extends State<Wgt2> {
+  int age = 24;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Directionality(
+        textDirection: TextDirection.rtl,
+        child: Container(
+          alignment: Alignment.center,
+          color: Colors.blueAccent,
+          child: MaterialButton(
+            onPressed: () {
+              setState(() {
+                age++;
+              });
+            },
+            color: Colors.yellowAccent,
+            child: Text("Age is: ${age.toString()}"),
+          ),
+        ));
   }
 }
