0d4dd5420fb567de7000aec6fdd592399b6a47fb
Sina <SinaKordestani@gmail.com>
2024-03-23 18:55:40 +0330

    Use Builder Widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
-
 double width = 0;
 void main() {
   // runApp(MaterialApp(home: Text("Sina TH",textAlign: TextAlign.center,style:TextStyle(color: Colors.red) ,),));
@@ -23,55 +22,57 @@ class MyWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Directionality(
-        textDirection: TextDirection.rtl,
-        child: MaterialApp(
-            debugShowCheckedModeBanner: false,
-            home: Scaffold(
-              appBar: AppBar(
-                title: const Align(
-                  alignment: Alignment.centerRight,
-                  child: Text(
-                    "سینا",
-                    style: TextStyle(fontFamily: "MyFont"),
-                  ),
-                ),
-                centerTitle: false,
-                elevation: 10,
-                actions: [
-                  Padding(
-                    padding: const EdgeInsets.fromLTRB(6, 5, 50, 5),
-                    // padding: EdgeInsets.all(8),
-                    child: InkWell(
-                      child: const Icon(Icons.access_alarm),
+    return Builder(
+      builder: (context) => Directionality(
+            textDirection: TextDirection.rtl,
+            child: MaterialApp(
+                debugShowCheckedModeBanner: false,
+                home: Scaffold(
+                  appBar: AppBar(
+                    title: const Align(
+                      alignment: Alignment.centerRight,
+                      child: Text(
+                        "سینا",
+                        style: TextStyle(fontFamily: "MyFont"),
+                      ),
+                    ),
+                    centerTitle: false,
+                    elevation: 10,
+                    actions: [
+                      Padding(
+                        padding: const EdgeInsets.fromLTRB(6, 5, 50, 5),
+                        // padding: EdgeInsets.all(8),
+                        child: InkWell(
+                          child: const Icon(Icons.access_alarm),
+                          onTap: () {
+                            if (kDebugMode) {
+                              print("Alarm BTN Clicked!!");
+                            }
+                          },
+                        ),
+                      )
+                    ],
+                    // ignore: prefer_const_constructors
+                    leading: InkWell(
+                      // child: const Icon(Icons.search),
+                      child: Image.asset('images/nasim-logo.png'),
                       onTap: () {
                         if (kDebugMode) {
-                          print("Alarm BTN Clicked!!");
+                          print("Search BTN Clicked!!1");
                         }
                       },
                     ),
-                  )
-                ],
-                // ignore: prefer_const_constructors
-                leading: InkWell(
-                  // child: const Icon(Icons.search),
-                  child: Image.asset('images/nasim-logo.png'),
-                  onTap: () {
-                    if (kDebugMode) {
-                      print("Search BTN Clicked!!1");
-                    }
-                  },
-                ),
-              ),
-              body: Center(
-                  child: MaterialButton(
-                      onPressed: () {},
-                      child: const Text(
-                        "Sina",
-                        style: TextStyle(
-                            fontSize: 20,
-                            color: Color.fromARGB(255, 100, 100, 100)),
-                      ))),
-            )));
+                  ),
+                  body: Center(
+                      child: MaterialButton(
+                          onPressed: () {},
+                          child: const Text(
+                            "Sina",
+                            style: TextStyle(
+                                fontSize: 20,
+                                color: Color.fromARGB(255, 100, 100, 100)),
+                          ))),
+                ))),
+    );
   }
 }
