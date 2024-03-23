ad262d4fca9b9a085ee677683c109c76f0c587ba
Sina <SinaKordestani@gmail.com>
2024-03-23 16:45:30 +0330

    Right Alignment Titile and Set Padding for Icon

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,15 @@
 import 'package:flutter/material.dart';
 
+double width = 0;
 void main() {
   // runApp(MaterialApp(home: Text("Sina TH",textAlign: TextAlign.center,style:TextStyle(color: Colors.red) ,),));
+  FractionallySizedBox(
+    widthFactor: 0.65, // between 0 and 1
+    heightFactor: 1.0,
+    child: Container(
+      color: Colors.red,
+    ),
+  );
 
   runApp(const MyWidget(
     key: Key("Sina"),
@@ -13,23 +21,38 @@ class MyWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        // debugShowCheckedModeBanner: false ,
-        home: Scaffold(
-      appBar: AppBar(
-        title: const Text("Sina"),
-        centerTitle: false,
-        // ignore: prefer_const_constructors
-        leading: Icon(Icons.access_alarm),
-      ),
-      body: Center(
-          child: MaterialButton(
-              onPressed: () {},
-              child: const Text(
-                "Sina",
-                style: TextStyle(
-                    fontSize: 20, color: Color.fromARGB(255, 100, 100, 100)),
-              ))),
-    ));
+    const double x = 56;
+    return Directionality(
+        textDirection: TextDirection.rtl,
+        child: MaterialApp(
+            debugShowCheckedModeBanner: false,
+            home: Scaffold(
+              appBar: AppBar(
+                title: const Align(
+                  alignment: Alignment.centerRight,
+                  child: Text("سینا"),
+                ),
+                centerTitle: false,
+                elevation: 10,
+                actions: const [
+                  Padding(
+                    padding: EdgeInsets.fromLTRB(6, 5, 50, 5),
+                    // padding: EdgeInsets.all(8),
+                    child: Icon(Icons.access_alarm),
+                  )
+                ],
+                // ignore: prefer_const_constructors
+                // leading: Icon(Icons.access_alarm),
+              ),
+              body: Center(
+                  child: MaterialButton(
+                      onPressed: () {},
+                      child: const Text(
+                        "Sina",
+                        style: TextStyle(
+                            fontSize: 20,
+                            color: Color.fromARGB(255, 100, 100, 100)),
+                      ))),
+            )));
   }
 }
